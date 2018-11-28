c59d3db135e881ac53a0e5c7eee976ee66842232
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-28 21:25:36 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,22 @@
 // Code your JavaScript / jQuery solution here
+var currentGame = 0;
+var turn = 0;
+
+var WIN_COMBOS = [
+     [0,1,2],
+     [3,4,5],
+     [6,7,8],
+     [0,3,6],
+     [1,4,7],
+     [2,5,8],
+     [0,4,8],
+     [2,4,6]
+  ]
+
+  function player() {
+    return turn % 2 ? "O" : "X"
+  }
+
+  function updateState() {
+    
+  }
